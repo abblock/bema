edb28a5b1828c9de11937ddbe57167bd8e840a9c
Adam Block <blockadam@microsoft.com>
2025-06-24 13:56:34 +0000

    removed unnecessary script

--- hf_login.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Login to the Hugging Face model hub
-
-/home/aiscuser/.local/bin/huggingface-cli login --token $(cat .hf_token)
-
-
-# wandb login $(cat .wandb_token) --host  https://microsoft-research.wandb.io
